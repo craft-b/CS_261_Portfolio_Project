76ccfbe14e3a5fbe93b7d48da30d4bc9fcbdfe60
craft-b <59632574+craft-b@users.noreply.github.com>
2023-03-06 22:43:30 -0500

    Update min_heap.py
    Refactored code smells

--- min_heap.py
+++ min_heap.py
@@ -10,7 +10,6 @@ from a5_include import *
 class MinHeapException(Exception):
     """
     Custom exception to be used by MinHeap class
-    DO NOT CHANGE THIS CLASS IN ANY WAY
     """
     pass
 
@@ -20,7 +19,6 @@ class MinHeap:
     def __init__(self, start_heap=None):
         """
         Initializes a new MinHeap
-        DO NOT CHANGE THIS METHOD IN ANY WAY
         """
         self.heap = DynamicArray()
         # populate MH with initial values (if provided)
@@ -32,14 +30,12 @@ class MinHeap:
     def __str__(self) -> str:
         """
         Return MH content in human-readable form
-        DO NOT CHANGE THIS METHOD IN ANY WAY
         """
         return 'HEAP ' + str(self.heap)
 
     def is_empty(self) -> bool:
         """
         Return True if no elements in the heap, False otherwise
-        DO NOT CHANGE THIS METHOD IN ANY WAY
         """
         return self.heap.length() == 0
     
@@ -48,26 +44,19 @@ class MinHeap:
         Adds new object to Heap;
         Maintains Heap properties
         """
-
-        # Put new element at the end of the array.
         self.heap.append(node)
 
         if self.heap.length() == 1:
             return
 
-        else:
-            # Compute the inserted element’s parent index((i - 1) / 2).
-            par_index = (((self.heap.length()-1)-1)//2)
-            inserted_child = self.heap.length() - 1
-
-            # Compare the value of the inserted element with the value of its parent.
-            while self.heap[inserted_child] < self.heap[par_index]:
-                self.heap[inserted_child], self.heap[par_index] = self.heap[par_index], self.heap[inserted_child]
-                inserted_child = par_index
-                if inserted_child == 0:
-                    return
-                par_index = ((inserted_child - 1)//2)
-            return
+        inserted_child = self.heap.length() - 1
+        while inserted_child > 0:
+            par_index = (inserted_child - 1) // 2
+            if self.heap[inserted_child] >= self.heap[par_index]:
+                break
+            self.heap[inserted_child], self.heap[par_index] = \
+                self.heap[par_index], self.heap[inserted_child]
+            inserted_child = par_index
 
     def get_min(self) -> object:
         """
@@ -83,38 +72,20 @@ class MinHeap:
         Removes minimum element in heap;
         Returns removed value
         """
-
         if self.is_empty():
             raise MinHeapException
 
         first_element = self.heap[0]
 
-        # Replace the value f the first element in the array with the value of the last element and
-        self.heap[0] = self.heap[self.heap.length() - 1]
+        # Replace the value of the first element in the array with the value 
+        # of the last element.
+        self.heap[0] = self.heap.pop()
 
-        # Remove the last element.
-        self.heap.pop()
+        # Call helper function to heapify/shift down larger values
+        self.shift_down(0)
 
-        # Starting index
-        i = 0
-
-        # If the array is not empty(i.e., it started with more than one element)
-        if not self.is_empty():
-
-            # Compute the indices of the children of the replacement element (2 * i + 1 and 2 * i + 2).
-            left_child = (2 * i) + 1
-            right_child = (2 * i) + 2
-
-            if right_child >= self.heap.length() and left_child >= self.heap.length():
-                return first_element
-
-            # Call helper function to heapify/shift down
-            # larger values
-            self.shift_down(i)
-
-            return first_element
         return first_element
-
+    
     def build_heap(self, da: DynamicArray) -> None:
         """
         Receives dynamic array with objects in any order;
@@ -125,29 +96,21 @@ class MinHeap:
         self.heap = da
 
         # Determine start index
-        i = (da.length() // 2) - 1
+        i = da.length() // 2 - 1
 
-        while (i > -1):
+        while i >= 0:
             self.shift_down(i)
-            i = i - 1
+            i -= 1
 
     def shift_down(self, i):
         """
         Helper function that percolates nodes down the tree
         Takes index
         """
-        while (i * 2) <= self.heap.length():
-
+        while (i * 2) < self.heap.length():
             mc = self.min_child(i)
-
-            if i * 2 + 2 >= self.heap.length() and i * 2 + 1 >= self.heap.length():
-                return
-
             if self.heap[i] > self.heap[mc]:
-                tmp = self.heap[i]
-                self.heap[i] = self.heap[mc]
-                self.heap[mc] = tmp
-
+                self.heap[i], self.heap[mc] = self.heap[mc], self.heap[i]
             i = mc
 
     def min_child(self, i):
@@ -155,23 +118,13 @@ class MinHeap:
         Helper function to determine minimum child;
         Takes index
         """
+        left_child = i * 2 + 1
+        right_child = i * 2 + 2
+        if right_child >= self.heap.length():
+            return left_child
+        return left_child if self.heap[left_child] <= \
+            self.heap[right_child] else right_child
 
-        # right child >= length, left child only leaf node
-        if i * 2 + 2 >= self.heap.length() and i * 2 + 1 < self.heap.length():
-            return i * 2 + 1
-
-        # left child >= length, right child only leaf
-        if i * 2 + 2 < self.heap.length() and i * 2 + 1 >= self.heap.length():
-            return i * 2 + 2
-
-        # both children are nodes
-        if i * 2 + 2 < self.heap.length() and i * 2 + 1 < self.heap.length():
-
-            if self.heap[i * 2 + 1] <= self.heap[i * 2 + 2]:
-                return i * 2 + 1
-
-            if self.heap[i * 2 + 2] <= self.heap[i * 2 + 1]:
-                return i * 2 + 2
 
 # BASIC TESTING
 if __name__ == '__main__':
